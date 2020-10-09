4b03e53414c5a5ab25ac56aebe8ab804594ddb53
Manuel Sánchez <msanchez@MacBook-Pro-de-Manuel.local>
2020-10-09 12:17:59 +0200

    Update deploy + src

---
+++ src/TestingADT/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Azure.DigitalTwins.Core;
+using Azure.Identity;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using Azure;
+using Azure.DigitalTwins.Core.Serialization;
+using System.Text.Json;
+
+namespace TestingADT
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            string clientId = "<clientId>";
+            string tenantId = "<tenatId>";
+            string adtInstanceUrl = "https://yourinstance";
+            var credentials = new InteractiveBrowserCredential(tenantId, clientId);
+            DigitalTwinsClient client = new DigitalTwinsClient(new Uri(adtInstanceUrl), credentials);
+            Console.WriteLine($"Service client created – ready to go");
+
+            Console.WriteLine();
+            Console.WriteLine($"Upload a model");
+            var typeList = new List<string>();
+            string dtdl = File.ReadAllText("Models/SampleModel.json");
+            typeList.Add(dtdl);
+
+            // Upload the model to the service
+            try {
+                await client.CreateModelsAsync(typeList);
+            } catch (RequestFailedException rex) {
+                Console.WriteLine($"Load model: {rex.Status}:{rex.Message}");
+            }
+            // Read a list of models back from the service
+            AsyncPageable<ModelData> modelDataList = client.GetModelsAsync();
+            await foreach (ModelData md in modelDataList)
+            {
+                Console.WriteLine($"Type name: {md.DisplayName}: {md.Id}");
+            }
+
+            // Initialize twin data
+            BasicDigitalTwin twinData = new BasicDigitalTwin();
+            twinData.Metadata.ModelId = "dtmi:com:contoso:SampleModel;1";
+            twinData.CustomProperties.Add("data", $"Hello World!");
+    
+            string prefix="sampleTwin-";
+            for(int i=0; i<3; i++) {
+                try {
+                    twinData.Id = $"{prefix}{i}";
+                    await client.CreateDigitalTwinAsync($"{prefix}{i}", JsonSerializer.Serialize(twinData));
+                    Console.WriteLine($"Created twin: {prefix}{i}");
+                } catch(RequestFailedException rex) {
+                    Console.WriteLine($"Create twin error: {rex.Status}:{rex.Message}");  
+                }
+            }
+
+            // Connect the twins with relationships
+            await CreateRelationship(client, "sampleTwin-0", "sampleTwin-1");
+            await CreateRelationship(client, "sampleTwin-0", "sampleTwin-2");
+
+            //List the relationships
+            await ListRelationships(client, "sampleTwin-0");
+
+            // Run a query    
+            AsyncPageable<string> result = client.QueryAsync("Select * From DigitalTwins");
+            await foreach (string twin in result)
+            {
+                object jsonObj = JsonSerializer.Deserialize<object>(twin);
+                string prettyTwin = JsonSerializer.Serialize(jsonObj, new JsonSerializerOptions { WriteIndented = true });
+                Console.WriteLine(prettyTwin);
+                Console.WriteLine("---------------");
+            }
+        }
+
+        public async static Task CreateRelationship(DigitalTwinsClient client, string srcId, string targetId)
+        {
+            var relationship = new BasicRelationship
+            {
+                TargetId = targetId,
+                Name = "contains"
+            };
+        
+            try
+            {
+                string relId = $"{srcId}-contains->{targetId}";
+                await client.CreateRelationshipAsync(srcId, relId, JsonSerializer.Serialize(relationship));
+                Console.WriteLine("Created relationship successfully");
+            }
+            catch (RequestFailedException rex) {
+                Console.WriteLine($"Create relationship error: {rex.Status}:{rex.Message}");
+            }
+        }
+        
+        public async static Task ListRelationships(DigitalTwinsClient client, string srcId)
+        {
+            try {
+                AsyncPageable<string> results = client.GetRelationshipsAsync(srcId);
+                Console.WriteLine($"Twin {srcId} is connected to:");
+                await foreach (string rel in results)
+                {
+                    var brel = JsonSerializer.Deserialize<BasicRelationship>(rel);
+                    Console.WriteLine($" -{brel.Name}->{brel.TargetId}");
+                }
+            } catch (RequestFailedException rex) {
+                Console.WriteLine($"Relationship retrieval error: {rex.Status}:{rex.Message}");   
+            }
+        }
+    }
+}
+
